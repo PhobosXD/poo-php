e211875cfe033382304598a0d8b87a4753f96c9b
Igor Emerick <igorbarbosaemerick@gmail.com>
2021-03-09 13:23:24 -0300

    Estrutura inicial da classe Produtos

--- database/Produto.php
+++ database/Produto.php
@@ -1,3 +1,65 @@
 <?php
 
 declare(strict_types=1);
+
+class Produto
+{
+    private $connection;
+
+    public function __construct()
+    {
+        try {
+            $this->connection = new PDO(
+                'mysql:host=localhost;dbname=teste-php',
+                'root',
+                '!12345Asd'
+            );
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+            die();
+        }
+    }
+
+    public function list() : void
+    {
+        $sql = 'select * from produtos';
+        
+        foreach ($this->connection->query($sql) as $key => $value) {
+            echo 'ID: '.$value['id'].'<br>Nome: '.$value['nome'].'<hr>';
+        }
+    }
+
+    public function insert() : int
+    {
+        $sql = 'insert into produtos(nome) value(?)';
+        $prepare = $this->connection->prepare($sql);
+
+        $prepare->bindParam(1, $_GET['nome']);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+
+    public function update() : int
+    {
+        $sql = 'update produtos set nome = ? where id = ?';
+        $prepare = $this->connection->prepare($sql);
+
+        $prepare->bindParam(1, $_GET['nome']);
+        $prepare->bindParam(2, $_GET['id']);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+
+    public function delete() : int
+    {
+        $sql = 'delete from produtos where id = ?';
+        $prepare = $this->connection->prepare($sql);
+
+        $prepare->bindParam(1, $_GET['id']);
+        $prepare->execute();
+
+        return $prepare->rowCount();
+    }
+}
--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
 
-include 'ContaBancaria.php';
+include 'database/Produto.php';
 
-$conta = new ContaBancaria('Nu Bank', 'Cazuza', '1234', '12345678-9', 0);
+$produto = new Produto();
 
-echo $conta->getSaldo();
-echo PHP_EOL;
-
-$conta->deposito(300);
-
-echo $conta->getSaldo();
-echo PHP_EOL;
-
-$conta->saque(200);
-
-echo $conta->getSaldo();
-echo PHP_EOL;
+$produto->list();
