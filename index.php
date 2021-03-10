21c858a4903bbc059daa6d4e66230aaf82e601db
Igor Emerick <igorbarbosaemerick@gmail.com>
2021-03-09 23:29:29 -0300

    Centralizando funções em index.php

--- index.php
+++ index.php
@@ -4,4 +4,24 @@ include 'database/Produto.php';
 
 $produto = new Produto();
 
-$produto->list();
+switch ($_GET['operation']) {
+    case 'list':
+        echo '<h1>Produtos:</h1>';
+
+        foreach ($produto->list() as $value) {
+            echo 'ID: '.$value['id'].'<br>Nome: '.$value['nome'].'<hr>';
+        }
+    break;
+
+    case 'insert':
+        echo $produto->insert('Produto01');
+    break;
+
+    case 'update':
+        echo $produto->update('Produto02', 2);
+    break;
+
+    case 'delete':
+        echo $produto->delete(1);
+    break;
+}
